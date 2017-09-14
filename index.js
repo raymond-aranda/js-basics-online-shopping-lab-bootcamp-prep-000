45894c6ca3efc010e3741c9dbf95029516c625a0
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-14 23:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  if (!cart.length) {
+    console.log("Your shopping cart is empty.")
+  }
 }
 
 function total() {
