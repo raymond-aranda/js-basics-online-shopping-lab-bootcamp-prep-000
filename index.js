764a7951c1d80d4bd6351e9f4595cd2d7503fe16
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-14 23:17:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
   var price = Math.floor(Math.random() * 100) + 1;
   cart.push({ [item]: price })
 
-  return cart
+  console.log(`${item} has been added to your cart.`);
 }
 
 function viewCart() {
