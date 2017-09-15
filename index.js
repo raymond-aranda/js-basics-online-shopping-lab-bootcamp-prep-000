a0f7b7ab477577e852784f5e8943af0ffbfcbe98
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-15 02:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,5 +77,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
