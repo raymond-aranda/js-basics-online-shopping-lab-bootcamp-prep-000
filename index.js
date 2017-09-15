cc916f7352714446717847b97f24dbedc60e5a6f
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-15 02:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,5 +79,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
+    return false;
   }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
 }
